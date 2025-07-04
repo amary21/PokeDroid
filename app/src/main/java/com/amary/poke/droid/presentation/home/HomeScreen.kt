e0f4743d1482b3ccfba5a4bb7c28e7848ebec50b
T-Amary <taufik.amary@gmail.com>
2025-07-04 21:37:09 +0700

    feat: Add empty BackHandler to HomeScreen
    This commit adds an empty `BackHandler` to the `HomeScreen.kt`.
    
    This is likely a placeholder for future functionality related to handling back button presses within this screen.

--- app/src/main/java/com/amary/poke/droid/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/amary/poke/droid/presentation/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.amary.poke.droid.presentation.home
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -55,6 +56,8 @@ fun HomeScreen(
 
     var searchQuery by remember { mutableStateOf("") }
     var isSearchActive by remember { mutableStateOf(false) }
+    
+    BackHandler {  }
 
     Scaffold(
         topBar = {
